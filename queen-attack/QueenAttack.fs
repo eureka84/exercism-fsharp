96206aafbca46a128751d5bff300d53253db22bb
eureka84 <angelosciarra@ymail.com>
2020-05-23 22:55:22 +0200

    queen attack - refactor and |> usage

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -1,23 +1,20 @@
 ﻿module QueenAttack
 
+let toOption bool = if bool then Some() else None
+
 let create (position: int * int) =
-    let (|ValidPos|_|) input =
-        if List.contains input [ 0 .. 7 ] then Some() else None
+    let (|ValidPos|_|) input = List.contains input [ 0 .. 7 ] |> toOption
     match position with
     | (ValidPos, ValidPos) -> true
     | _ -> false
 
 let canAttack (queen1: int * int) (queen2: int * int) =
-    let (|SameDiagonal|_|) (p1, p2) =
-        let (x1, y1) = p1
-        let (x2, y2) = p2
-        if abs (x1 - x2) = abs (y1 - y2) then Some() else None
+    let (|SameDiagonal|_|) ((x1, y1), (x2, y2)) =
+        abs (x1 - x2) = abs (y1 - y2) |> toOption
 
-    let (|SameColumn|_|) (p1, p2) =
-        if snd p1 = snd p2 then Some() else None
+    let (|SameColumn|_|) ((_,y1), (_,y2)) = y1 = y2 |> toOption
 
-    let (|SameRow|_|) (p1, p2) =
-        if fst p1 = fst p2 then Some() else None
+    let (|SameRow|_|) ((x1, _), (x2, _)) = x1 = x2 |> toOption
 
     match queen1, queen2 with
     | SameRow
