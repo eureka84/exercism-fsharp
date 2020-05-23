8986f8304597fd6a5e91ac2094d8e3c5b33c8282
eureka84 <angelosciarra@ymail.com>
2020-05-24 00:41:01 +0200

    raindrops - refcator with option, without sequence

--- raindrops/Raindrops.fs
+++ raindrops/Raindrops.fs
@@ -3,12 +3,6 @@
 let private testEmit divisor sound =
     fun n ->
         if n % divisor = 0 then Some sound else None
-        
-let sequence (l:list<option<'T>>): option<list<'T>> =
-    let folder (el: option<'T>) (acc: option<list<'T>>): option<list<'T>> =
-          let addEl (curr: list<'T>) = el |> Option.map (fun s -> s::curr)
-          acc |> Option.bind addEl         
-    List.foldBack folder l (Some [])
 
 let convert (number: int): string =
     let soundsToTest =
@@ -16,13 +10,15 @@ let convert (number: int): string =
           testEmit 5 "Plang"
           testEmit 7 "Plong" ]
 
-    let maybeAListOfSounds =
+    let sounds =
         soundsToTest
         |> List.map (fun emitFor -> emitFor number)
         |> List.filter Option.isSome
-        |> sequence      
+        |> List.map Option.get
 
-    match maybeAListOfSounds with
-    | None
-    | Some [] -> string number
-    | Some sounds -> sounds |> List.toSeq |> String.concat "" 
+    match sounds with
+    | [] -> string number
+    | _ ->
+        sounds
+        |> List.toSeq
+        |> String.concat ""
