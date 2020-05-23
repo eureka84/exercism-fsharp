24c81877a8ce80bc9ea946ff94ebf3adee3fc2e1
eureka84 <angelosciarra@ymail.com>
2020-05-23 22:39:32 +0200

    queen attack - refactor with partial active patterns

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -8,16 +8,16 @@ let create (position: int * int) =
     | _ -> false
 
 let canAttack (queen1: int * int) (queen2: int * int) =
-    let (|SameDiagonal|NotSameDiagonal|) (p1, p2) =
+    let (|SameDiagonal|_|) (p1, p2) =
         let (x1, y1) = p1
         let (x2, y2) = p2
-        if abs (x1 - x2) = abs (y1 - y2) then SameDiagonal else NotSameDiagonal
+        if abs (x1 - x2) = abs (y1 - y2) then Some() else None
 
-    let (|SameColumn|NotSameColumn|) (p1, p2) =
-        if snd p1 = snd p2 then SameColumn else NotSameColumn
+    let (|SameColumn|_|) (p1, p2) =
+        if snd p1 = snd p2 then Some() else None
 
-    let (|SameRow|NotSameRow|) (p1, p2) =
-        if fst p1 = fst p2 then SameRow else NotSameRow
+    let (|SameRow|_|) (p1, p2) =
+        if fst p1 = fst p2 then Some() else None
 
     match queen1, queen2 with
     | SameRow
