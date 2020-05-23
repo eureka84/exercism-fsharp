f5c41e677e85c04cc1a7907c61ed1f796edf3d00
eureka84 <angelosciarra@ymail.com>
2020-05-24 01:15:21 +0200

    accumulate

---
+++ accumulate/Accumulate.fs
@@ -0,0 +1,8 @@
+﻿module Accumulate
+
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
+    let rec innerMap incoming output = 
+        match incoming with
+        | [] -> output
+        | x::xs ->  innerMap xs (func x :: output) 
+    innerMap input [] |> List.rev 
