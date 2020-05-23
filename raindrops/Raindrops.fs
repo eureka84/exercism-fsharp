011eec6a871b538e6a2f703fb8f654edc9734415
eureka84 <angelosciarra@ymail.com>
2020-05-23 23:43:42 +0200

    raindrops

---
+++ raindrops/Raindrops.fs
@@ -0,0 +1,21 @@
+﻿module Raindrops
+
+let private testEmit divisor sound =
+    fun n ->
+        if n % divisor = 0 then sound else ""
+
+let convert (number: int): string =
+    let soundsToTest =
+        [ testEmit 3 "Pling"
+          testEmit 5 "Plang"
+          testEmit 7 "Plong" ]
+
+    let sounds =
+        soundsToTest
+        |> List.map (fun emitFor -> emitFor number)
+        |> List.toSeq
+        |> String.concat ""
+
+    match sounds with
+    | "" -> string number
+    | _ -> sounds
