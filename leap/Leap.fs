ca7d073b870f926a566fb44e7f2d35f992ada3be
eureka84 <angelosciarra@ymail.com>
2020-05-23 20:18:28 +0200

    leap

---
+++ leap/Leap.fs
@@ -0,0 +1,6 @@
+﻿module Leap
+
+let private isDivisibleBy divisor = fun x -> x % divisor = 0
+let leapYear (year: int): bool =
+       isDivisibleBy 400 year
+    || ((isDivisibleBy 4 year ) && not (isDivisibleBy 100 year))
