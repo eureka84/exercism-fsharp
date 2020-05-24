6fb118c23b508ab1097c0a08a5c69580264abab9
eureka84 <angelosciarra@ymail.com>
2020-05-24 20:22:54 +0200

    grade school - refactor rewritten roster

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -4,18 +4,24 @@ type School = Map<int, string list>
 
 let empty: School = Map.empty
 
+let studentsForGrade (grade:int) (school:School): string list option =
+    Map.tryFind grade school
+    
 let add (student: string) (grade: int) (school: School): School =
-    match Map.tryFind grade school with
+    match studentsForGrade grade school with
     | Some students ->
-        Map.add grade (student :: students) school
+        let sortedStudents = student :: students |> List.sort
+        Map.add grade sortedStudents school
     | None ->
         Map.add grade (student :: []) school
 
 let roster (school: School): string list =
-    let folder acc _ v = List.sort v |> (@) acc
-    Map.fold folder [] school
+    school
+    |> Map.toSeq
+    |> Seq.collect snd
+    |> Seq.toList    
 
 let grade (number: int) (school: School): string list =
-    match Map.tryFind number school with
+    match studentsForGrade number school with
     | Some students -> List.sort students
     | None -> []
