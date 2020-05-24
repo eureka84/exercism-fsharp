afcfd9db0c9a4f0e96f89848c205eabaabd7dbb1
eureka84 <angelosciarra@ymail.com>
2020-05-24 10:51:27 +0200

    raindrops - option with fold

--- raindrops/Raindrops.fs
+++ raindrops/Raindrops.fs
@@ -4,18 +4,18 @@ let private testEmit divisor sound =
     fun n ->
         if n % divisor = 0 then Some sound else None
 
+let combine (o1: string option) (o2: string option) =
+    match o1, o2 with
+    | Some s1, Some s2 -> [s1; s2] |> String.concat "" |> Some
+    | Some s1, None -> Some s1
+    | None, Some s2 -> Some s2
+    | None, None -> None
+    
 let convert (number: int): string =
     let soundsToTest =
         [ testEmit 3 "Pling"
           testEmit 5 "Plang"
           testEmit 7 "Plong" ]
-
-    let combine (o1: string option) (o2: string option) =
-        match o1, o2 with
-        | Some s1, Some s2 -> [s1; s2] |> String.concat "" |> Some
-        | Some s1, None -> Some s1
-        | None, Some s2 -> Some s2
-        | None, None -> None
     
     let sounds =
         soundsToTest
