2caeccbe6c9369d6e2c8e094ad7904c436e9e4f0
eureka84 <angelosciarra@ymail.com>
2020-05-26 19:34:25 +0200

    clock - refactor

--- clock/Clock.fs
+++ clock/Clock.fs
@@ -5,21 +5,14 @@ type Clock =
       Minutes: int }
 
 let create (hours: int) (minutes: int): Clock =
-    let m, dh = 
-        let m1 = minutes % 60
-        let dh1 = minutes / 60
-        if m1 < 0 then
-            60 + m1, dh1 - 1
-        else
-            m1, dh1
-    let h =
-        let h1 = (hours + dh) % 24
-        if h1 < 0 then
-            24 + h1
-        else
-            h1
-    { Hours = h
-      Minutes = m }
+    let rec loop h m =
+        match h, m with
+        | h, m when m >= 60 -> loop (h + 1) (m - 60) 
+        | h, m when m < 0 -> loop (h - 1)  (m + 60)
+        | h, m when h >= 24 -> loop (h - 24) m
+        | h, m when h < 0 -> loop (h + 24) m
+        | _ -> { Hours = h ; Minutes = m }
+    loop hours minutes
 
 let add minutes (clock: Clock) =
     create clock.Hours (clock.Minutes + minutes)
