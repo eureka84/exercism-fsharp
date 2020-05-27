59e70ecb27b6800120d77342ada20867e0ef0227
eureka84 <angelosciarra@ymail.com>
2020-05-27 21:43:00 +0200

    robot - refactor

--- robot-simulator/RobotSimulator.fs
+++ robot-simulator/RobotSimulator.fs
@@ -16,37 +16,42 @@ let create direction position =
     { direction = direction
       position = position }
 
+let changeDirection robot direction =
+    { robot with direction = direction }
+
 let private rotateRight robot =
     match robot.direction with
-    | North -> { robot with direction = East }
-    | East -> { robot with direction = South }
-    | South -> { robot with direction = West }
-    | West -> { robot with direction = North }
-    
+    | North -> East
+    | East -> South
+    | South -> West
+    | West -> North
+    |> changeDirection robot
+
 let private rotateLeft robot =
     match robot.direction with
-    | North -> { robot with direction = West }
-    | East -> { robot with direction = North }
-    | South -> { robot with direction = East }
-    | West -> { robot with direction = South }
+    | North -> West
+    | East -> North
+    | South -> East
+    | West -> South
+    |> changeDirection robot
+
+let changePosition robot position =
+    { robot with position = position }
 
 let private moveForward robot =
-    match robot with
-    | { direction = North; position = x, y } ->
-        { robot with position = x,  y + 1 }
-    | { direction = East; position = x, y } ->
-        { robot with position = x  + 1, y }
-    | { direction = South; position = x, y } ->
-        { robot with position = x, y - 1 }
-    | { direction = West; position = x, y } ->
-        { robot with position = x - 1, y }
+    let { direction = d; position = x, y } = robot
+    match d with
+    | North -> x, y + 1 
+    | East -> x + 1, y 
+    | South -> x, y - 1 
+    | West -> x - 1, y
+    |> changePosition robot
 
 let moveRobot (robot: Robot) (command: char) =
     match command with
     | 'R' -> rotateRight robot
     | 'L' -> rotateLeft robot
     | 'A' -> moveForward robot
+
 let move (instructions: string) (robot: Robot) =
-    instructions
-    |> Seq.fold moveRobot robot 
-    
+    instructions |> Seq.fold moveRobot robot
