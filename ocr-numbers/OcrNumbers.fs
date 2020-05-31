27c84ccaf2fd6d54613699dfa4d68df102ff8243
eureka84 <angelosciarra@ymail.com>
2020-05-31 23:03:28 +0200

    ocr - refactor

--- ocr-numbers/OcrNumbers.fs
+++ ocr-numbers/OcrNumbers.fs
@@ -16,17 +16,6 @@ let toDigit = function
 let toOption b =
     if b then Some() else None
 
-let isValid (input: string list): string list option =
-    let (|Has4Rows|_|) input = toOption (List.length input = 4)
-    let lengthDivisibleBy3 str = String.length str % 3 = 0
-    let (|ColumnsNumberIsDivisibleBy3|_|) (input: string list) =
-        toOption (List.forall lengthDivisibleBy3 input)
-
-    match input with
-    | Has4Rows & ColumnsNumberIsDivisibleBy3 -> Some input
-    | _ -> None
-
-
 let zip4 (l1: string list) (l2: string list) (l3: string list) (l4: string list) =
     let zip (ll1: string list) (ll2: string list list): string list list =
         List.zip ll1 ll2 |> List.map (fun p -> fst p::snd p)
@@ -34,7 +23,6 @@ let zip4 (l1: string list) (l2: string list) (l3: string list) (l4: string list)
     |> List.map (fun p -> fst p::snd p::[])
     |> zip l2
     |> zip l1
-    
     
 let getDigitsToConvert (line: string list): string list list =
     let chunked: string list list =
@@ -52,6 +40,16 @@ let concat opt1 opt2 =
     | Some a, Some b -> Some(sprintf "%s,%s" a b)
     | _ -> None
 
+let isValid (input: string list): string list option =
+    let (|Has4Rows|_|) input = toOption (List.length input = 4)
+    let lengthDivisibleBy3 str = String.length str % 3 = 0
+    let (|ColumnsNumberIsDivisibleBy3|_|) (input: string list) =
+        toOption (List.forall lengthDivisibleBy3 input)
+
+    match input with
+    | Has4Rows & ColumnsNumberIsDivisibleBy3 -> Some input
+    | _ -> None
+    
 let convert (input: string list) =
     List.chunkBySize 4 input
     |> List.map (isValid >> Option.map convertSingleLine)
