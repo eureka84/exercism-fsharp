1f932f4930f18bc57f7c2d597e1c5b910773a14b
eureka84 <angelosciarra@ymail.com>
2020-06-03 20:39:29 +0200

    piglatin - refactor

--- pig-latin/PigLatin.fs
+++ pig-latin/PigLatin.fs
@@ -2,33 +2,29 @@
 
 open System.Text.RegularExpressions
 
-type Rule =
-    { apply: string -> bool
-      transform: string -> string }
-
 let VOWEL_PATTERN = "^([aeiou]|yt|xr).*"
 let CONSONANTS_PATTERN = "^(squ|thr|sch|ch|qu|th|rh|[^aeiou])(.*)"
 
-let startsWithAVowel: Rule =
-    { apply = fun s -> Regex.Match(s, VOWEL_PATTERN).Success
-      transform = fun s -> s + "ay" }
+let (|Vowel|_|) input =
+    if Regex.Match(input, VOWEL_PATTERN).Success then
+        Some()
+    else
+        None
 
-let startsWithAConsonant: Rule =
-    { apply = fun s -> Regex.Match(s, CONSONANTS_PATTERN).Success
-      transform =
-          fun s ->
-              let m = Regex.Match(s, CONSONANTS_PATTERN)
-              let prefix, postfix = m.Groups.[1].Value, m.Groups.[2].Value
-              postfix + prefix + "ay" }
+let (|Consonant|_|) input =
+    let m = Regex.Match(input, CONSONANTS_PATTERN)
+    if m.Success then
+        Some(m.Groups.[1].Value, m.Groups.[2].Value)
+    else
+        None
 
-let rules = [ startsWithAVowel; startsWithAConsonant ]
-
-let applyRules s =
-    List.tryFind (fun (r: Rule) -> r.apply s) rules
-    |> Option.map (fun (r: Rule) -> r.transform s)
-    |> Option.defaultValue s
+let translateWord s =
+    match s with
+    | Vowel -> s + "ay"
+    | Consonant (prefix, postfix) -> postfix + prefix + "ay"
+    | _ -> s
     
 let translate (input: string) =
     input.Split(" ")
-    |> Array.map applyRules
+    |> Seq.map translateWord
     |> String.concat " "
