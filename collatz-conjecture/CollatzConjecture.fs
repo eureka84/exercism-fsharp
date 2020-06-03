f7bed374c9692fb08ca8aafc5c0a9cee015ea016
eureka84 <angelosciarra@ymail.com>
2020-06-03 23:05:44 +0200

    collatz conjecture

---
+++ collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,10 @@
+﻿module CollatzConjecture
+
+let steps (number: int): int option =
+    let rec loop n steps =
+        match n with
+        | 1 -> Some steps
+        | _ when n <= 0 -> None
+        | _ when n % 2 = 0 -> loop (n / 2) (steps + 1)
+        | _ when n % 2 = 1 -> loop (3 * n + 1) (steps + 1)
+    loop number 0
