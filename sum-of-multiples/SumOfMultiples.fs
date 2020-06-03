796650e34a3ff36e52d4c4030a095f4d18307964
eureka84 <angelosciarra@ymail.com>
2020-06-03 18:35:58 +0200

    sum of multiples

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,9 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int): int =
+    let factors = numbers |> List.filter (fun x -> x <> 0)
+    let isXDivisibleByY x y = x % y = 0
+    let isDivisibleByOneOfFactors x = List.exists (isXDivisibleByY x) factors 
+    [ 1 .. (upperBound - 1) ]
+    |> List.filter isDivisibleByOneOfFactors 
+    |> List.sum
