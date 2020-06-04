68cf2cf11924110e6e420a0f57abe5034e6ed514
eureka84 <angelosciarra@ymail.com>
2020-06-04 21:14:54 +0200

    ArmstrongNumbers

---
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,12 @@
+﻿module ArmstrongNumbers
+
+let digitsOf number =
+    number |> string |> Seq.map (fun x -> new string ([|x|])) |> Seq.map int
+
+let isArmstrongNumber (number: int): bool =
+    let digits = digitsOf number
+    let numOfDigits = Seq.length digits
+
+    digits
+    |> Seq.map (fun x -> pown x numOfDigits)
+    |> Seq.sum = number
