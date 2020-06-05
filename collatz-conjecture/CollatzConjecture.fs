c3901cd81231b8899025163885259ae9fef49d8e
eureka84 <angelosciarra@ymail.com>
2020-06-05 20:34:12 +0200

    collatz conjecture - refactor

--- collatz-conjecture/CollatzConjecture.fs
+++ collatz-conjecture/CollatzConjecture.fs
@@ -1,11 +1,14 @@
 ﻿module CollatzConjecture
 
+let (|Even|Odd|) input =
+    if input % 2 =0 then Even else Odd
+
 let steps (number: int): int option =
     let rec computeSteps n steps =
         match n with
         | 1 -> steps
-        | _ when n % 2 = 0 -> computeSteps (n / 2) (steps + 1)
-        | _ when n % 2 = 1 -> computeSteps (3 * n + 1) (steps + 1)
+        | Even -> computeSteps (n / 2) (steps + 1)
+        | Odd -> computeSteps (3 * n + 1) (steps + 1)
     match number with
     | n when n <= 0 -> None
     | n -> Some <| computeSteps n 0
