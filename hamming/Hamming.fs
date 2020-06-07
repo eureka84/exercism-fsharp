2d1f84381ebbf64f3edfe60ffb107052b7c42e82
eureka84 <angelosciarra@ymail.com>
2020-06-07 20:53:34 +0200

    hamming

---
+++ hamming/Hamming.fs
@@ -0,0 +1,13 @@
+﻿module Hamming
+
+let private charList s = s |> Seq.toList
+
+let actualDistance (strand1: string) (strand2: string) =
+    ((charList strand1), (charList strand2))
+    ||> List.zip
+    |>  List.sumBy (fun (c1, c2) -> if c1 <> c2 then 1 else 0)
+
+let distance (strand1: string) (strand2: string): int option =
+    if strand1.Length <> strand2.Length
+    then None
+    else Some(actualDistance strand1 strand2)
