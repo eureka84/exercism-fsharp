e116362c2974fcfba5a7bae3fff0d8a17bcaf786
eureka84 <angelosciarra@ymail.com>
2020-06-07 16:56:35 +0200

    grains

--- grains/Grains.fs
+++ grains/Grains.fs
@@ -5,13 +5,13 @@ let square (n: int): Result<uint64, string> =
     then Ok(pown 2UL (n - 1))
     else Error "square must be between 1 and 64"
 
-let combine (r1: Result<uint64, string>) (r2: Result<uint64, string>): Result<uint64, string> =
+let combine (innerCombine: 'a -> 'a ->'a)(r1: Result<'a, 'e>) (r2: Result<'a, 'e>)=
     match r1, r2 with
-    | Ok s1, Ok s2 -> Ok(s1 + s2)
+    | Ok s1, Ok s2 -> Ok(innerCombine s1 s2)
     | Error _, _ -> r1
     | _, Error _ -> r2
 
 let total: Result<uint64, string> =
     [ 1 .. 64 ]
     |> List.map square
-    |> List.reduce combine
+    |> List.reduce (combine (+))
