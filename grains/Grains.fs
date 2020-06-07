d8524f614fb986fa8f65a2717c813871461640b5
eureka84 <angelosciarra@ymail.com>
2020-06-07 16:41:39 +0200

    refactor

--- grains/Grains.fs
+++ grains/Grains.fs
@@ -1,7 +1,9 @@
 ﻿module Grains
 
 let square (n: int): Result<uint64, string> =
-    if n >= 1 && n <= 64 then Ok(pown 2UL (n - 1)) else Error "square must be between 1 and 64"
+    if n >= 1 && n <= 64
+    then Ok(pown 2UL (n - 1))
+    else Error "square must be between 1 and 64"
 
 let combine (r1: Result<uint64, string>) (r2: Result<uint64, string>): Result<uint64, string> =
     match r1, r2 with
