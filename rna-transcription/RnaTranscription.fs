3134e549ec66b403b2f484f26ccd3db324a2532e
eureka84 <angelosciarra@ymail.com>
2020-06-07 15:57:04 +0200

    rna

---
+++ rna-transcription/RnaTranscription.fs
@@ -0,0 +1,12 @@
+﻿module RnaTranscription
+
+let private nucleotideComplement = function
+    | 'C' -> 'G'
+    | 'A' -> 'U'
+    | 'T' -> 'A'
+    | 'U' -> 'A'
+    | 'G' -> 'C'
+    | _ -> failwith "Unknown nucleotide"
+
+let toRna (dna: string): string =
+    dna |> Seq.map nucleotideComplement |> System.String.Concat
