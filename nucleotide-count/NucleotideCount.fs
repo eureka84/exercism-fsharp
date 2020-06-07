6f96779c34e1a9deae91312f0a0f8255acba158c
eureka84 <angelosciarra@ymail.com>
2020-06-08 00:53:03 +0200

    NucleotideCount

---
+++ nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,16 @@
+﻿module NucleotideCount
+
+let updateNucleotides (c: char) (nucleotides: Map<char, int>): Map<char, int> option =
+    nucleotides.TryFind c |> Option.map (fun count -> nucleotides.Add(c, count + 1))
+
+let nucleotideCounts (strand: string): Option<Map<char, int>> =
+    let nucleotides =
+        [ ('A', 0)
+          ('C', 0)
+          ('G', 0)
+          ('T', 0) ]
+        |> Map.ofList
+    let folder acc x = acc |> Option.bind (updateNucleotides x) 
+    
+    strand
+    |> Seq.fold folder (Some nucleotides)
