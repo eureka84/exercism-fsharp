d9345c7783aa5325e062f7ff856db4898a85ab38
eureka84 <angelosciarra@ymail.com>
2020-06-08 22:13:02 +0200

    pangram

---
+++ pangram/Pangram.fs
@@ -0,0 +1,10 @@
+﻿module Pangram
+
+open System.Text.RegularExpressions
+
+let isPangram (input: string): bool =
+    Regex.Replace(input.ToLower(), "[^a-z]", "")
+    |> Seq.distinct
+    |> Seq.length
+    |> (=) 26
+        
