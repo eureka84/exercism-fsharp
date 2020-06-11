bdecd97310ee12031f27b8d0c81c3b0b9d445b72
eureka84 <angelosciarra@ymail.com>
2020-06-11 22:35:37 +0200

    darts

--- darts/Darts.fs
+++ darts/Darts.fs
@@ -4,15 +4,14 @@ let private square (x: double) = x * x
 let private distanceFromZero (x: double) (y: double): double =
     sqrt (square x + square y)
 
-let private toOption (b: bool) =
-    if b then Some() else None
+let (|OutOfTarget|InsideOuterCircle|InsideMiddleCircle|InsideInnerCircle|) (d: double) =
+    match d with
+    | _ when (d > 10.0) -> OutOfTarget
+    | _ when (d <= 10.0 && d > 5.0) -> InsideOuterCircle
+    | _ when (d <= 5.0 && d > 1.0) -> InsideMiddleCircle
+    | _ when (d <= 1.0) -> InsideInnerCircle
 
 let score (x: double) (y: double): int =
-    let (|OutOfTarget|_|) (d: double) = (d > 10.0) |> toOption
-    let (|InsideOuterCircle|_|) (d: double) = (d <= 10.0 && d > 5.0) |> toOption
-    let (|InsideMiddleCircle|_|) (d: double) = (d <= 5.0 && d > 1.0) |> toOption
-    let (|InsideInnerCircle|_|) (d: double) = (d <= 1.0) |> toOption
-
     match distanceFromZero x y with
     | OutOfTarget -> 0
     | InsideOuterCircle -> 1
