745735a47a65150526e348487554d5198cdff8d6
eureka84 <angelosciarra@ymail.com>
2020-06-11 22:46:10 +0200

    darts

--- darts/Darts.fs
+++ darts/Darts.fs
@@ -1,15 +1,14 @@
-﻿module Darts
+module Darts
 
 let private square (x: double) = x * x
-let private distanceFromCenter (x: double) (y: double): double =
-    sqrt (square x + square y)
+let private distanceFromCenter (x: double) (y: double): double = sqrt (square x + square y)
 
 let (|OutOfTarget|InsideOuterCircle|InsideMiddleCircle|InsideInnerCircle|) (x: double, y: double) =
     match distanceFromCenter x y with
-    | d when (d > 10.0) -> OutOfTarget
     | d when (d <= 10.0 && d > 5.0) -> InsideOuterCircle
     | d when (d <= 5.0 && d > 1.0) -> InsideMiddleCircle
     | d when (d <= 1.0) -> InsideInnerCircle
+    | _ -> OutOfTarget
 
 let score (x: double) (y: double): int =
     match (x, y) with
