063e725ca79325c1a8817ad0bd77e7a0a5b83ee1
eureka84 <angelosciarra@ymail.com>
2020-06-11 22:13:23 +0200

    etl

---
+++ etl/Etl.fs
@@ -0,0 +1,12 @@
+﻿module Etl
+
+let private toPairsList (score: int, letters: char list): (char * int) list =
+    letters
+    |> List.map System.Char.ToLower 
+    |> List.map (fun letter -> (letter, score))
+let transform (scoresWithLetters: Map<int, char list>): Map<char, int> =
+    scoresWithLetters
+    |> Map.toSeq
+    |> Seq.map toPairsList
+    |> Seq.concat
+    |> Map.ofSeq
