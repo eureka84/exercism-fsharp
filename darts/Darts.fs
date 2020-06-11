1df53f51623daa3032f4f7fd8994c4f79ae4f89b
eureka84 <angelosciarra@ymail.com>
2020-06-11 22:29:32 +0200

    darts

---
+++ darts/Darts.fs
@@ -0,0 +1,20 @@
+﻿module Darts
+
+let private square (x: double) = x * x
+let private distanceFromZero (x: double) (y: double): double =
+    sqrt (square x + square y)
+
+let private toOption (b: bool) =
+    if b then Some() else None
+
+let score (x: double) (y: double): int =
+    let (|OutOfTarget|_|) (d: double) = (d > 10.0) |> toOption
+    let (|InsideOuterCircle|_|) (d: double) = (d <= 10.0 && d > 5.0) |> toOption
+    let (|InsideMiddleCircle|_|) (d: double) = (d <= 5.0 && d > 1.0) |> toOption
+    let (|InsideInnerCircle|_|) (d: double) = (d <= 1.0) |> toOption
+
+    match distanceFromZero x y with
+    | OutOfTarget -> 0
+    | InsideOuterCircle -> 1
+    | InsideMiddleCircle -> 5
+    | InsideInnerCircle -> 10
