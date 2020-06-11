5a9644f1007385bc5d548179440eeaf5741e71a2
eureka84 <angelosciarra@ymail.com>
2020-06-11 22:42:13 +0200

    darts

--- darts/Darts.fs
+++ darts/Darts.fs
@@ -4,15 +4,15 @@ let private square (x: double) = x * x
 let private distanceFromCenter (x: double) (y: double): double =
     sqrt (square x + square y)
 
-let (|OutOfTarget|InsideOuterCircle|InsideMiddleCircle|InsideInnerCircle|) (d: double) =
-    match d with
-    | _ when (d > 10.0) -> OutOfTarget
-    | _ when (d <= 10.0 && d > 5.0) -> InsideOuterCircle
-    | _ when (d <= 5.0 && d > 1.0) -> InsideMiddleCircle
-    | _ when (d <= 1.0) -> InsideInnerCircle
+let (|OutOfTarget|InsideOuterCircle|InsideMiddleCircle|InsideInnerCircle|) (x: double, y: double) =
+    match distanceFromCenter x y with
+    | d when (d > 10.0) -> OutOfTarget
+    | d when (d <= 10.0 && d > 5.0) -> InsideOuterCircle
+    | d when (d <= 5.0 && d > 1.0) -> InsideMiddleCircle
+    | d when (d <= 1.0) -> InsideInnerCircle
 
 let score (x: double) (y: double): int =
-    match distanceFromCenter x y with
+    match (x, y) with
     | OutOfTarget -> 0
     | InsideOuterCircle -> 1
     | InsideMiddleCircle -> 5
