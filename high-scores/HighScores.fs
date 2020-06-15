5cd11a312feb98f32737934d2a5624425e7e6cae
eureka84 <angelosciarra@ymail.com>
2020-06-15 21:48:06 +0200

    High scores

---
+++ high-scores/HighScores.fs
@@ -0,0 +1,13 @@
+﻿module HighScores
+
+let scores (values: int list): int list = values
+
+let latest (values: int list): int = List.last values
+
+let personalBest (values: int list): int = List.max values    
+
+let personalTopThree (values: int list): int list =
+    let sorted = values |> List.sortDescending
+    match sorted  with
+    | l when List.length l < 3 -> l
+    | l -> l |> List.take 3
