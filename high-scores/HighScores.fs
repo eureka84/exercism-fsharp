5771dfe25c8299d33db4fb18258ab3f64be24b38
eureka84 <angelosciarra@ymail.com>
2020-06-15 21:50:54 +0200

    High scores -refactor

--- high-scores/HighScores.fs
+++ high-scores/HighScores.fs
@@ -7,7 +7,4 @@ let latest (values: int list): int = List.last values
 let personalBest (values: int list): int = List.max values    
 
 let personalTopThree (values: int list): int list =
-    let sorted = values |> List.sortDescending
-    match sorted  with
-    | l when List.length l < 3 -> l
-    | l -> l |> List.take 3
+    values |> List.sortDescending  |> List.truncate 3
