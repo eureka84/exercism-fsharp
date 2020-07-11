9576baf833a2a057f3e2a93d1421c835caee11b9
eureka84 <angelosciarra@ymail.com>
2020-07-11 10:33:43 +0200

    error handling

---
+++ error-handling/ErrorHandling.fs
@@ -0,0 +1,28 @@
+﻿module ErrorHandling
+
+open System
+
+let handleErrorByThrowingException() = failwith "Exception"
+
+let handleErrorByReturningOption (input: string) =
+    try
+        Some (int input)
+    with
+        _ -> None
+
+let handleErrorByReturningResult (input: string) =
+    try
+        Ok (int input)
+    with
+        _ -> Error "Could not convert input to integer"
+
+let bind (switchFunction: 'a -> Result<'a, 'e>) (twoTrackInput: Result<'a, 'e>): Result<'a, 'e> =
+    match twoTrackInput with
+    | Ok x -> switchFunction x
+    | Error _ -> twoTrackInput 
+
+let cleanupDisposablesWhenThrowingException (resource: IDisposable) =
+    try
+        failwith "Exception"
+    finally
+        resource.Dispose()
